e5e46bb24ea9679a32ae07f7d994aaa793fa0c59
DongY00n-KIM <dongyoon.roy.kim@gmail.com>
2022-08-25 21:00:37 -0700

    Add Sub and Main menu, and buttons.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,27 @@ searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+    //hide badge
+    // badgeEl.style.display = 'none';
+    gsap.to(badgeEl, 0.4, {
+      display: 'none',
+      opacity: 0
+    });
+  }
+  else{
+    //show badge
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, 0.4, {
+      display: 'block',
+      opacity: 1
+    });
+  }
+}, 300));
+
